c173a836100f4d18a2adb739cf0df8d7991a170d
Jozef Maxted <jozef.maxted@stratumfive.com>
2018-07-23 09:33:55 +0100

    Particles following vector field

--- src/App.vue
+++ src/App.vue
@@ -6,96 +6,100 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import { Noise } from 'noisejs';
-import Victor from 'victor';
-import { Particle } from './Particle';
+import { Noise } from 'noisejs'
+import Victor from 'victor'
+import { Particle } from './Particle'
 
-const noise = new Noise(Math.random());
+const noise = new Noise(Math.random())
 
 export default {
   name: 'app',
   components: {
     HelloWorld
   },
-  data() {
+  data () {
     return {
       canvas: null,
       ctx: null,
       width: 0,
       height: 0,
-      scale: 20,
+      scale: 40,
       cols: 0,
       rows: 0,
       field: [],
+      numParticles: 800,
       particles: [],
-      time: 0,
+      time: 0
     }
   },
-  mounted() {
-    this.canvas = this.$refs.canvas;
-    this.ctx = this.canvas.getContext('2d');
-    this.width = this.canvas.width = window.innerWidth;
-    this.height = this.canvas.height = window.innerHeight;
-    this.setColRows(this.scale, this.width, this.height);
-    this.createParticles();
-    this.animate();
+  mounted () {
+    this.canvas = this.$refs.canvas
+    this.ctx = this.canvas.getContext('2d')
+    this.width = this.canvas.width = window.innerWidth
+    this.height = this.canvas.height = window.innerHeight
+    this.setColRows(this.scale, this.width, this.height)
+    this.createParticles()
+    this.generateField(0);
+    this.animate()
   },
   methods: {
-    setColRows(scale, width, height) {
-      this.cols = Math.floor(width / scale);
-      this.rows = Math.floor(height / scale);
+    setColRows (scale, width, height) {
+      this.cols = Math.floor(width / scale)
+      this.rows = Math.floor(height / scale)
     },
-    generateField(time) {
+    generateField (time) {
       for (let y = 0; y < this.rows; y++) {
         for (let x = 0; x < this.cols; x++) {
-          const index = (x + y * this.width);
-          const xOffset = Math.floor( x * this.scale );
-          const yOffset =Math.floor( y * this.scale );
+          const index = (x + y * this.cols)
+          const xOffset = Math.floor(x * this.scale)
+          const yOffset = Math.floor(y * this.scale)
 
-          const angle = Math.abs( noise.simplex3(x / this.scale, y / this.scale, this.time) ) * 360;
-          const speed = Math.abs( noise.simplex3(x / this.scale, y / this.scale, this.time) ) * this.scale;
-          const angleVector = new Victor(0, speed);
-          angleVector.rotateByDeg(angle);
-          
-          this.field[index] = angleVector;
+          const angle = noise.simplex3(x / this.scale, y / this.scale, this.time) * 360
+          const speed = Math.abs(noise.simplex3(x / this.scale, y / this.scale, this.time)) * this.scale
+          // const angleVector = new Victor(0, speed)
+          const angleVector = new Victor(0, this.scale)
+          angleVector.rotateByDeg(angle)
+
+          this.field[index] = angleVector
         }
       }
     },
-    draw() {
-      for (let y = 0; y < this.rows; y++) {
-        for (let x = 0; x < this.cols; x++) {
-          const index = (x + y * this.width);
-          const angleVector = this.field[index];
+    draw () {
+      // for (let y = 0; y < this.rows; y++) {
+      //   for (let x = 0; x < this.cols; x++) {
+      //     const index = (x + y * this.cols)
+      //     const angleVector = this.field[index]
 
-          const xOffset = Math.floor( x * this.scale );
-          const yOffset = Math.floor( y * this.scale );
-         
-          const currentPos = new Victor(xOffset, yOffset);
-          currentPos.add(angleVector);
+      //     const xOffset = Math.floor(x * this.scale)
+      //     const yOffset = Math.floor(y * this.scale)
 
-          this.ctx.beginPath();
-          this.ctx.moveTo(xOffset, yOffset);
-          this.ctx.lineTo(currentPos.x, currentPos.y);
-          this.ctx.stroke();
-        }
-      }
+      //     const currentPos = new Victor(xOffset, yOffset)
+      //     currentPos.add(angleVector)
+
+      //     this.ctx.beginPath()
+      //     this.ctx.moveTo(xOffset, yOffset)
+      //     this.ctx.lineTo(currentPos.x, currentPos.y)
+      //     this.ctx.stroke()
+      //     this.ctx.fillRect(currentPos.x, currentPos.y, 3, 3);
+      //   }
+      // }
 
       this.particles.forEach((particle) => {
-        particle.follow(this.field);
-        particle.update();
-        this.ctx.fillRect(particle.pos.x, particle.pos.y,5,5);
-      });
+        particle.follow(this.field)
+        particle.update()
+        particle.show(this.ctx)
+      })
     },
-    animate() {
-      this.time += 0.01;
-      this.ctx.clearRect(0,0,this.width,this.height);
-      this.generateField(this.time);
-      this.draw();
-      requestAnimationFrame(this.animate.bind(this));
+    animate () {
+      this.time += 0.001
+      this.ctx.clearRect(0, 0, this.width, this.height)
+      this.generateField(this.time)
+      this.draw()
+      requestAnimationFrame(this.animate.bind(this))
     },
-    createParticles() {
-      for (let i = 0; i < 2; i++) {
-        this.particles.push(new Particle(this.width, this.height, this.scale, this.cols, this.rows))
+    createParticles () {
+      for (let i = 0; i < this.numParticles; i++) {
+        this.particles.push(new Particle(this.width, this.height, this.scale, this.rows, this.cols))
       }
     }
   }
--- src/Particle.js
+++ src/Particle.js
@@ -5,29 +5,32 @@ export class Particle {
     this.pos = new Victor(Math.random() * width, Math.random() * height)
     this.vel = new Victor(0, 0)
     this.acc = new Victor(0, 0)
-    console.log(this.acc)
-    this.maxspeed = 4
+    this.maxspeed = 2
     this.h = 0
     this.prevPos = new Victor(0, 0).copy(this.pos)
     this.scl = scale
     this.cols = cols
     this.rows = rows
+    this.height = height
+    this.width = width
+    this.forceIndex = 0
   }
 
   update () {
     this.vel.add(this.acc)
-    this.vel.limit(this.maxspeed)
+    this.limit(this.vel, this.maxspeed)
     // // console.log(this.acc);
     this.pos.add(this.vel)
     this.acc.multiply(new Victor(0, 0))
   }
 
   follow (vectors) {
-    var x = Math.floor(this.pos.x / this.scl)
-    var y = Math.floor(this.pos.y / this.scl)
-    var index = x + y * this.cols
-    var force = vectors[index]
-    console.log(index)
+    const x = Math.floor(this.pos.x / this.scl)
+    const y = Math.floor(this.pos.y / this.scl)
+    const index = x + y * this.cols
+    const force = vectors[index]
+    // window.forceIndex = index
+    // console.log(force.angleDeg())
     if (force) {
       this.applyForce(force)
     }
@@ -35,41 +38,62 @@ export class Particle {
 
   applyForce (force) {
     this.acc.add(force)
+    // this.vel = force.clone().normalize()
   }
 
-//   this.show = function() {
-//     stroke(this.h, 255, 255, 25);
-//     this.h = this.h + 1;
-//     if (this.h > 255) {
-//       this.h = 0;
-//     }
-//     strokeWeight(1);
-//     line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-//     this.updatePrev();
-//   }
+  show (ctx) {
+    // stroke(this.h, 255, 255, 25);
+    // this.h = this.h + 1;
+    // if (this.h > 255) {
+    //   this.h = 0;
+    // }
+    // strokeWeight(1);
+    // line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    // const currentPos = new Victor(0, 0)
+    // currentPos.add(angleVector)
 
-  // updatePrev() {
-  //     this.prevPos.x = this.pos.x;
-  //     this.prevPos.y = this.pos.y;
-  // }
+    // this.ctx.beginPath()
+    // this.ctx.moveTo(xOffset, yOffset)
+    // this.ctx.lineTo(currentPos.x, currentPos.y)
+    // this.ctx.stroke()
+    // this.ctx.fillRect(currentPos.x, currentPos.y, 3, 3);
 
-  // edges() {
-  //     if (this.pos.x > width) {
-  //     this.pos.x = 0;
-  //     this.updatePrev();
-  //     }
-  //     if (this.pos.x < 0) {
-  //     this.pos.x = width;
-  //     this.updatePrev();
-  //     }
-  //     if (this.pos.y > height) {
-  //     this.pos.y = 0;
-  //     this.updatePrev();
-  //     }
-  //     if (this.pos.y < 0) {
-  //     this.pos.y = height;
-  //     this.updatePrev();
-  //     }
+    // ctx.fillStyle = 'rgba(0,0,0,1)'
+    ctx.fillRect(this.pos.x, this.pos.y, 0.5, 0.5)
+    this.updatePrev()
+    this.edges()
+  }
 
-  // }
+  updatePrev () {
+    this.prevPos.x = this.pos.x
+    this.prevPos.y = this.pos.y
+  }
+
+  edges () {
+    if (this.pos.x > this.width) {
+      this.pos.x = 0
+      this.updatePrev()
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = this.width
+      this.updatePrev()
+    }
+    if (this.pos.y > this.height) {
+      this.pos.y = 0
+      this.updatePrev()
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = this.height
+      this.updatePrev()
+    }
+  }
+
+  limit (vector, max) {
+    const maxVector = new Victor(max, max)
+    if (vector.lengthSq() > maxVector.lengthSq()) {
+      vector
+        .normalize()
+        .multiply(maxVector)
+    }
+  }
 }
